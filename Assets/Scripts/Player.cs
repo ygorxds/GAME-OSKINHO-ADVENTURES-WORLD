8bfa9ffdd478f78318c85dfae2849c6b5358947b
ygorxds <ygordesa2010@hotmail.com>
2023-05-27 15:36:05 -0300

    create: character start developmenyt

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,15 +4,26 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
+
+    public float speed;
+
+    private Rigidbody2D rig;
+    private Vector2 direction;
+
+    private void Start()
     {
-        
+        rig = GetComponent<Rigidbody2D>();
     }
 
-    // Update is called once per frame
-    void Update()
-    {
 
+    private void Update()
+    {
+        direction = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
     }
+
+    private void FixedUpdate()
+    {
+        rig.MovePosition(rig.position + direction * speed * Time.fixedDeltaTime);
+    }
+
 }
